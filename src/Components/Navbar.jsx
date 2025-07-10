d8b4997ed7086c58b1ae837a34e8ccc18152fd14
khaironnahar4 <khaironnahar247@gmail.com>
2025-07-10 11:48:47 +0600

    Update navbar links to correct project and experience sections

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skill</a></li>
-        <li><a href="#client">Client</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#projects">Project</a></li>
         <li><a href="#contact">Contact</a></li>
     </>
     return (
