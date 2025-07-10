b0c615a1e6b3abaa2451851a9b23844ddef51b68
khaironnahar4 <khaironnahar247@gmail.com>
2025-07-10 11:46:58 +0600

    Update footer component layout and text alignment

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="px-4">
       <div className="w-full h-[1px] bg-gray-500 mt-24"></div>
-      <div className="md:flex justify-between mt-4 max-w-[1400px] mx-auto sm:hidden">
+      <div className="md:flex justify-between mt-4 max-w-[1400px] mx-auto hidden">
         <p className="text-3xl text-gray-500 ">DM Younus </p>
         <ul className="flex gap-4 text-gray-500 text-xl">
           {footerLinks.map((item, index) => {
@@ -26,7 +26,7 @@ const Footer = () => {
           })}
         </ul>
       </div>
-      <p className="max-w-[1400px] mx-auto text-right mt-2 pb-12 text-sm text-gray-500">
+      <p className="max-w-[1400px] mx-auto text-center sm:text-right mt-2 pb-12 text-sm text-gray-500">
         © {new Date().getFullYear()} DM Younus | All Rights Reserved.
       </p>
     </div>
