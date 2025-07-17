322ba7f5ddc76fb45ba41c72cf8fe7e3bc929694
safin33221 <safin33221@gmail.com>
2025-07-18 01:10:44 +0600

    Update contact section

--- src/Components/contact/ContactMain.jsx
+++ src/Components/contact/ContactMain.jsx
@@ -7,10 +7,10 @@ const ContactMain = () => {
   return (
     <div
       id="contact"
-      className="max-w-[1400px] mx-auto items-center justify-center mt-[100px] px-4 "
+      className="max-w-[1400px] mx-auto items-center justify-center mt-[50px]  "
     >
       <SectionTitle title={`Contact`}/>
-      <div className="  grid grid-cols-1 lg:grid-cols-2  bg-stone-900 p-8 rounded-2xl lg:flex-row flex-col">
+      <div className="  grid grid-cols-1 lg:grid-cols-2  bg-zinc-900 p-8  lg:flex-row flex-col">
         <ContactRight />
         <ContactLeft />
       </div>
